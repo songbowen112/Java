ebde8f1fbafc1aff0b6072225bbb6d4cd93f5afc
songbowen <670317458@qq.com>
2020-09-11 21:11:25 +0800

    NoTask:init

---
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/colson/springcloud/entity/Payment.java
@@ -0,0 +1,13 @@
+package com.colson.springcloud.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment {
+
+    private Long id;
+}
